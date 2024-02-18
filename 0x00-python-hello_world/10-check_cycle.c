370384b87ad4e3fbbdc3ccc77e0ffa152e138b0d
johnitopaisah <johnitopaisah@gmail.com>
2024-02-17 22:41:07 -0800

    updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,48 +1,6 @@
 #include "lists.h"
 
 
-// /**
-//  * _realloc - Realloates a memory block
-//  * @ptr: The pointer to the previous memory block
-//  * @old_size: The size of the old memory block
-//  * @new_size: The size of the new memory block
-//  * 
-//  * Return: The pointer to the new memory block otherwise NULL
-// */
-// void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-// {
-// 	void *new_ptr;
-// 	unsigned int i;
-// 	unsigned int min_size = old_size < new_size ? old_size : new_size;
-
-// 	if (new_size == old_size)
-// 		return (ptr);
-// 	if (ptr != NULL)
-// 	{
-// 		if (new_size == 0)
-// 		{
-// 			free(ptr);
-// 			return (NULL);
-// 		}
-// 		new_ptr = malloc(new_size);
-// 		if (new_ptr == NULL)
-// 		{
-// 			for (i = 0; i < min_size; i++)
-// 				*((char *)new_ptr + 1) = *((char *)ptr + 1);
-// 			free(ptr);
-// 			return (new_ptr);
-// 		}
-// 		free(ptr);
-// 		return (NULL);
-// 	}
-// 	else
-// 	{
-// 		new_ptr = malloc(new_size);
-// 		return (new_ptr);
-// 	}
-// }
-
-
 /**
  * _realloc - Realloates a memory block
  * @ptr: The pointer to the previous memory block
