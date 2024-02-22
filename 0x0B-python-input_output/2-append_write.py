6be03f2a23db732a5002acec9f2d58366cf57089
johnitopaisah <johnitopaisah@gmail.com>
2024-02-22 04:35:59 +0100

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """Print the content of a UTF8 file to  stdout.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters apppended.
+    """
+    with open(filename, "a", encoding="utf-8")
