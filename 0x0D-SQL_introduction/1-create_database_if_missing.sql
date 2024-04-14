472a4189b6c077590833fae98d9147cd13413522
johnitopaisah <johnitopaisah@gmail.com>
2024-04-14 07:52:17 -0700

    create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---Create the database hbtn_0c_0 in my MySQL server
-CREATE DATABASE IF NOT EXITS "hbtn_0c_0";
+-- Create the database hbtn_0c_0 in my MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
