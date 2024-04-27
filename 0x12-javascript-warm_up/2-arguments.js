ef22243daade8d012f0b46214e5b374397ea7788
johnitopaisah <isahjohna@gmail.com>
2024-04-27 19:15:36 +0200

    aguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const count = process.argv.length
+const count = process.argv.length;
 console.log(count === 2 ? 'No argument found' : count === 3 ? 'Argument found' : 'Argument found');
