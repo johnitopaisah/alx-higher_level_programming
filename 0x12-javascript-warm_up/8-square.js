f60789294730b48a06cc7fce7873164c37a9e562
johnitopaisah <isahjohna@gmail.com>
2024-04-27 21:00:59 +0200

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+const num = Number(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let line = '';
+    for (let j = 0; j < num; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
