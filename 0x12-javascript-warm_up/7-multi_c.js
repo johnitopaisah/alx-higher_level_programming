51d2a051366f099ee84bfe1690044e9c83e48770
johnitopaisah <isahjohna@gmail.com>
2024-04-27 20:29:19 +0200

    I love c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num = Number(process.argv[2]);
+if (isNaN(num)) {
+    console.log('Missing numberr of occurences');
+} else {
+    for (let i = 0; i < num; i++) {
+        console.log('C is fun');
+    }
+}
